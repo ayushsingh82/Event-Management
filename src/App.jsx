9b04bfa18f3301e3684ea02c6957d0bc350ac3e7
Ayush Singh <ayushsinghmi711@gmail.com>
2023-12-30 06:00:27 +0530

    page import

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import EventList from './pages/EventList/EventList'
+import FilterEvent from './pages/FilterEvent/FilterEvent'
+import EventDetails from './pages/EventDetails/EventDetails'
+
+const router=createBrowserRouter([
+  {path:'/',element:<EventList/>},
+  {path:'/find-events',element:<FilterEvent/>},
+  {path:'/events/:id',element:<EventDetails/>},
+])
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/pages/FilterEvent/FilterEvent.jsx
@@ -0,0 +1,9 @@
+const FilterEvent=()=>{
+    return(
+        <div>
+            <h1>Filter Event</h1>
+        </div>
+    )
+}
+
+export default FilterEvent
