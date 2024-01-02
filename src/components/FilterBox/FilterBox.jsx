7fc5825619e1fb482f1cadd609066d7c16926348
Ayush Singh <ayushsinghmi711@gmail.com>
2024-01-02 14:14:24 +0530

    Data Render

--- src/components/FilterBox/FilterBox.jsx
+++ src/components/FilterBox/FilterBox.jsx
@@ -1,8 +1,54 @@
 import './FilterBox.css'
+
+const months=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const years=[2023,2024];
+
+const monthToDisplay=()=>{
+    return months.map((month,index)=>{
+        return (
+            <option key={index} value={month}>
+                {month}
+            </option>
+        )    
+    })
+}
+
+const yearsToDisplay=()=>{
+    return years.map((year,index)=>{
+        return(
+            <option key={index} value={year}>
+                {year}
+            </option>
+        )
+    })
+}
+
 const FilterBox=()=>{
     return (
         <div>
-            <h3>Filter Box</h3>
+           <form>
+            <label htmlFor='month'>Month:</label>
+            <select>
+              {monthToDisplay()}
+            </select>
+            <label htmlFor='year'>Year:</label>
+            <select>
+                {yearsToDisplay()}
+            </select>
+           </form>
         </div>
     )
 }
